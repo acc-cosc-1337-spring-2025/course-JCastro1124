ab175139ee5cabd415129f43a34561ce4b3c1cb7
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-10 23:46:19 +0000

    decimals

--- src/examples/01_data_types/02_decimals/main.cpp
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -1,5 +1,11 @@
+#include "decimals.h"
+
+using std::cout;
+
 int main()
 {
-
+    cout<<"Double(decimal) comp not reliable: ";
+    double result = add_to_double_1(0);
+    cout<<"Output: "<<result<<"\n";
     return 0;
 }
--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "int.h"
+#include "decimals.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
@@ -16,7 +17,11 @@ TEST_CASE("TEST substract numbers")
 /*test case echo_variable */
 
 /*test case add_to_double_1 with 0 as parameter*/
-
+TEST_CASE("test add to double .3 3 tiumes")
+{
+	REQUIRE(add_to_double_1(0) == .9);
+	REQUIRE(add_to_double_1(1) == 1.0);
+}
 /*test case add_to_double_1 with 1 as parameter*/
 
 /*test case add_to_double_2 with 0 as parameter*/
