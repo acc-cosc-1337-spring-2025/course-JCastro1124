0e351cf4cf7eb1a1ee63c3dec62c73b023d66580
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-11 00:03:57 +0000

    auto

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,14 @@
+#include<iostream>
+using std::cout;
+
 int main()
 {
+    auto num = 5;
+    auto num2 = 59.6;
+    auto letteer = "f";
 
+    cout<<letteer<<"\n";
+    cout<<num<<"\n";
+    cout<<num2<<"\n";
     return 0;
 }
