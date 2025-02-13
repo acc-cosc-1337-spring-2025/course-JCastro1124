cc9fb2abe1035292ebe95f365752360c6a91d7c9
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-13 00:25:20 +0000

    constant

--- src/examples/02_expressions/04_constants/main.cpp
+++ src/examples/02_expressions/04_constants/main.cpp
@@ -1,5 +1,17 @@
+#include <iostream>
+#include "constants.h"
 
+using std::cout;
+using std::cin;
 int main()
 {
+    cout<<RATE<<"\n";
+    auto amount = 0;
+    
+    cout<<"Enter num: ";
+    cin>>amount;
+
+    double result = calc_interset(amount):
+    cout<<"result: "<<result<<"\n";
     return 0;
 }
