d855c34f518ccf27c627cdb38e1faa6e05c588e7
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-13 00:18:14 +0000

    constant var

--- src/examples/02_expressions/04_constants/constants.h
+++ src/examples/02_expressions/04_constants/constants.h
@@ -2,3 +2,6 @@
 Create double value-return function get_area_of_circle with 
 double paramter (radius)
 */
+const double RATE = .05; // only change here
+
+double calc_interset(int amount);
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "expressions.h"
+#include "constants.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -15,4 +16,8 @@ TEST_CASE("Verify op pre 2")
 {
 	REQUIRE(op_pre_2(2,6,2) == 4);
 	REQUIRE(op_pre_2(2,10,2) == 6);
+}
+TEST_CASE("TEST constant")
+{
+	REQUIRE(calc_interset(100) == 5);
 }
