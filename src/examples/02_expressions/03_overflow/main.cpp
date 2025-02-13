d854eb48c7954e4d69a4dcb70dac8c40fb162234
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-13 00:00:18 +0000

    over

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -1,6 +1,13 @@
 
+#include <iostream>
+#include <limits.h>
+using std::cout;
 int main()
 {
+    int max_int = INT_MAX;
+    cout<<"Max int"<<max_int<<"\n";
 
+    max_int = max_int+1;
+    cout<<"Max int"<<max_int<<"\n";
     return 0;
 }
