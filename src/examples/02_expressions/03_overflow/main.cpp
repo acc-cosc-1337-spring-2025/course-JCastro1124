07151e61c7f2e1df2ec8648f371d5f675df0296f
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-13 00:09:45 +0000

    overflow/ under/ sizeoff

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -9,5 +9,15 @@ int main()
 
     max_int = max_int+1;
     cout<<"Max int"<<max_int<<"\n";
+    cout<<sizeof(max_int)<<"\n";
+
+    char letter = 'a';
+    cout<<sizeof(letter)<<"\n";
+
+    double num = 10.5;
+    cout<<sizeof(num)<<"\n";
+
+    float fv = 3.0E-47;
+    cout<<"v o fv: "<<fv<<"\n";
     return 0;
 }
