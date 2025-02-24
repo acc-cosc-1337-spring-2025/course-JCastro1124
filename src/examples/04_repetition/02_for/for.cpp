18a21556cfe85de238dfcf85fa7f35d0851fd1bf
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-24 23:52:29 +0000

    for loop

--- src/examples/04_repetition/02_for/for.cpp
+++ src/examples/04_repetition/02_for/for.cpp
@@ -1,17 +1,13 @@
 #include "for.h"
-#include<iostream>
+using std::cout;
+using std::cin;
 
-//Write code for function display_numbers that accepts a number and 
-//displays every number that precedes(starting from 1) it in a line
-//Example: display_numbers(5)
-/*
-RESULT:
 
-1
-2
-3
-4
-5
-
-*/
+void use_for_loop(int num)
+{
+    for(auto i=0;i<num;i++)
+    {
+        cout<<"Hello \n";
+    }
+}
 
--- src/examples/04_repetition/02_for/for.h
+++ src/examples/04_repetition/02_for/for.h
@@ -1,3 +1,7 @@
 //Write function prototype for a function named display_numbers 
 //that accepts an int and returns void
 
+#include<iostream>
+
+
+void use_for_loop(int num);
