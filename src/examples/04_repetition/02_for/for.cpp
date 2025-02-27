ff0af25d4c2467364f66839a2f0e5d7c07a51ad1
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-02-27 00:02:50 +0000

    for table

--- src/examples/04_repetition/02_for/for.cpp
+++ src/examples/04_repetition/02_for/for.cpp
@@ -11,3 +11,14 @@ void use_for_loop(int num)
     }
 }
 
+void multi_table_for(const int rows, const int cols)
+{
+    for(auto i = 0;i<rows;i++)
+    {
+        for( auto j = 0;j<cols;j++)
+        {
+            cout<<setw(4)<<(i + 1)*(j+1);
+        }
+        cout<<"\n";
+    }
+}
--- src/examples/04_repetition/02_for/for.h
+++ src/examples/04_repetition/02_for/for.h
@@ -2,6 +2,8 @@
 //that accepts an int and returns void
 
 #include<iostream>
-
+#include<iomanip>
 
 void use_for_loop(int num);
+
+void multi_table_for(const int rows, const int cols);
