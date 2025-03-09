e16b45b6c9e46b95e683d278bf8012c3a20306d1
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-03-09 01:58:54 +0000

    update 1 of hw5

--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,12 @@
 //add function(s) prototype here
+#include <iostream>
+#include<string>
+using std::string;
+
+const string content = "GC";
+
+double get_gc_content(string dna);
+
+string get_dna_complement(string dna);
+
+string reverse_string(string dna);
