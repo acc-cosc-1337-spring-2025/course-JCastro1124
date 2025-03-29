c1eaf96cdcec8c1464f1d2ff5f4d0145c4693882
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-03-29 21:07:55 +0000

    hw 6 unfinished(just in case)

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,45 @@
 //cpp
+#include "tic_tac_toe.h"
+
+bool tic_tac_toe::game_over()
+{
+    return false; //temporary
+}
+
+void tic_tac_toe::start_game(string first_player)
+{
+    player = first_player;
+    clear_board();
+}
+
+void tic_tac_toe::mark_board(int position)
+{
+    pegs[position-1] = player;
+    set_next_player();
+}
+
+
+
+
+
+//private functions
+void tic_tac_toe::clear_board()
+{
+    for (auto& peg: pegs)
+    {
+        peg = " ";//clears board
+    }
+    
+}
+
+void tic_tac_toe::set_next_player()
+{
+    if(player == "X")
+    {
+        player == "O";
+    }
+    if(player == "O")
+    {
+        player == "X";
+    }
+}
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,28 @@
 //h
+#include <iostream>
+#include <string>
+#include <vector>
+using std::vector;
+
+using std::string;
+
+
+class tic_tac_toe
+{
+public:
+    bool game_over();
+    void start_game(string first_player); //out
+    void mark_board(int position); //out
+    string get_player() const{return player;};  //in
+    void display_board() const; //
+private:
+    string player;
+    vector<string> pegs {9," "}; //in
+    void set_next_player(); //out
+    bool check_board_full(); //
+    void clear_board(); //out
+
+};
+
+
+
