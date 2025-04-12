95d207c8ceb5b55b5cec7ee8ce324fd0d1528730
JCastro1124 <jacob.castro4@g.austincc.edu>
2025-04-12 20:38:39 +0000

    HW8(unfinished)

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 using std::vector;
 using std::cout;
 using std::string;
 
+
+#ifndef tic_tac_toe_H
+#define tic_tac_toe_H
 
 class tic_tac_toe
 {
@@ -31,5 +35,5 @@ private:
     void set_winner(string win_player) {winner = win_player;};
 };
 
-
+#endif tic_tac_toe_H
 
--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,24 @@
 //h
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using std::vector;
+using std::cout;
+using std::string;
+
+#ifndef tic_tac_toe_manager_H
+#define tic_tac_toe_manager_H
+
+class tic_tac_toe_manager
+{
+public:
+
+
+private:
+
+
+};
+
+#endif tic_tac_toe_manager_H
